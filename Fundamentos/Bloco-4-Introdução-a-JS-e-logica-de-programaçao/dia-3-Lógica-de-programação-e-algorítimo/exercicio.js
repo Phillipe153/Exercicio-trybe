558bcdb522067b1cbf96313899127101506aa0ff
phillipe <rodrigues153@gmail.com>
2021-07-30 18:52:14 -0300

    adiciona exercicio 1

---
+++ Fundamentos/Bloco-4-Introdução-a-JS-e-logica-de-programaçao/dia-3-Lógica-de-programação-e-algorítimo/exercicio.js
@@ -0,0 +1,25 @@
+let n = Math.floor(Math.random ()*6+1) ;
+console.log(n);
+
+
+let coluna = null;
+
+// => 1 Quadrado com linhas e colunas iguais 
+let linha = [];
+linha.length = n;
+//coluna=n;
+
+
+
+for (i=0;i<n;i++)
+{
+   linha[i]='*';    
+   
+}
+for (i=0;i<n;i++)
+
+{
+   
+   console.log(linha);
+      
+}
