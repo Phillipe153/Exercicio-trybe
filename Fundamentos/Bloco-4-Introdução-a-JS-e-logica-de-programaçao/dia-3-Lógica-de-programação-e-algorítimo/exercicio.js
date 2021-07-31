8d6009226da1b1c7b7fc9e63e75c6b703553a3a9
phillipe <rodrigues153@gmail.com>
2021-07-30 22:15:46 -0300

    adiciona exercicios 1, 2, 3 e 4

--- Fundamentos/Bloco-4-Introdução-a-JS-e-logica-de-programaçao/dia-3-Lógica-de-programação-e-algorítimo/exercicio.js
+++ Fundamentos/Bloco-4-Introdução-a-JS-e-logica-de-programaçao/dia-3-Lógica-de-programação-e-algorítimo/exercicio.js
@@ -23,3 +23,68 @@ for (i=0;i<n;i++)
    console.log(linha);
       
 }
+// => triangulo de base 5
+console.log("");
+console.log("=> triangulo de base 5");
+console.log("");
+let linha2 = [];
+for (i=0;i<5;i++)
+{
+    linha2.length=i;
+    linha2[i]='*'; 
+    console.log(linha2);   
+}
+// => triangulo de base 5 invertido
+console.log("");
+console.log("=> triangulo de base 5 invertido");
+console.log("");
+let linha3 = [" "," "," "," "," "];
+
+
+for (i=5;i>=0;i--)
+
+{
+  if (linha3[i]!="*")
+    {
+        linha3[i]='*';
+        console.log(linha3);
+
+    }
+   
+    
+    
+
+}
+// =>  4 piramide com n asteriscos
+console.log("");
+console.log("=> 4 piramide com n asteriscos");
+console.log("");
+n = Math.floor(Math.random ()*6+1) ;
+if(n%2==0)
+{
+    n=n+1
+}
+console.log(n);
+
+
+let linha4=[];
+linha4.length=n;
+for (i=0;i<n;i++)
+    {
+        linha4[i]=" ";
+    }
+    console.log(n/2);
+    console.log(Math.ceil(n/2));
+    linha4[Math.ceil(n/2)] = 5;
+console.log(linha4);
+for (i=0;i<n;i++)
+{
+    
+    /*if(linha4[i]===" " || linha4[n-i])
+    {
+
+    }
+    linha4[(n/2)]="*";
+    console.log(linha4);*/
+
+}
