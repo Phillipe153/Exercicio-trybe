06898b130b174758258bf807af812633ddc34221
phillipe <rodrigues153@gmail.com>
2021-08-27 20:14:14 -0300

    adiciona exercicio 2 da part I

--- Fundamentos/Bloco-7-Introduçao-a-JavaScript-ES6-e-Testes-unitarios/dia-1-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/Bloco-7-Introduçao-a-JavaScript-ES6-e-Testes-unitarios/dia-1-let-const-arrow-functions-e-template-literals/script.js
@@ -10,3 +10,13 @@
       }
 
       testingScope(true);
+
+      const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+      oddsAndEvens.sort((a, b) => {
+        if (a>b) return 1;
+        if (a<b) return -1;
+        return 0;
+      })
+      
+      console.log(oddsAndEvens); // será necessário alterar essa linha 😉
