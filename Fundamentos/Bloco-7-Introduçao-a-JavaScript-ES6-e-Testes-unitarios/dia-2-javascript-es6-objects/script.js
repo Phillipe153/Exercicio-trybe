0d88fd81ac5a398244fab05ef39497d318902357
phillipe <rodrigues153@gmail.com>
2021-09-01 22:29:09 -0300

    Adiciona exercicios do 1 ao 4 da parte II

--- Fundamentos/Bloco-7-Introduçao-a-JavaScript-ES6-e-Testes-unitarios/dia-2-javascript-es6-objects/script.js
+++ Fundamentos/Bloco-7-Introduçao-a-JavaScript-ES6-e-Testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -52,3 +52,56 @@ const order = {
   orderModifier(order);
 
   //Parte II
+  const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  }
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  //Crie uma função para adicionar o turno da noite na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+  adicionaTurno = (object, key, value) => {
+    object[key]= value;
+
+  }
+  adicionaTurno (lesson2, "turno", 'noite');
+  console.log(lesson2);
+
+  //Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro
+
+  listarChaves = (object) => {
+    return Object.keys(object);
+
+  }
+
+  console.log(listarChaves(lesson2));
+
+  //Crie uma função para mostrar o tamanho de um objeto.
+
+  mostraTamanho = (object) => {
+    return Object.keys(object).length;
+  }
+
+ console.log(mostraTamanho(lesson3));
+
+ //Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+ listarChaves = (object) => {
+  return Object.values(object);
+
+}
+
+console.log(listarChaves(lesson3));
