8295f1f49cfda7a9a93f372d0962412a761362fa
phillipe <rodrigues153@gmail.com>
2021-09-05 19:21:24 -0300

    Adiciona esercicios do dia

---
+++ Fundamentos/Bloco-7-Introduçao-a-JavaScript-ES6-e-Testes-unitarios/dia-3-testes-unitarios/script.js
@@ -0,0 +1,126 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+
+ assert.strictEqual(sum(4, 5), 9);
+ assert.strictEqual(sum(0, 0), 0);
+ assert.throws(() => {
+  sum(4, '5');
+ });
+ assert.throws(() => {
+  sum(4, '5');
+ }, /^Error: parameters must be numbers$/);
+
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+//A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Verifique se o array passado por parâmetro não sofreu alterações
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+const myArray = [1, 2, 3, 4, 5];
+myRemove(myArray, 5);
+
+assert.deepStrictEqual(myArray, [1, 2, 3, 4, 5]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+
+//A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
+// Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
+
+
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+const arrayTeste = [1, 2, 3, 4]
+myRemoveWithoutCopy(arrayTeste, 4)
+assert.deepStrictEqual(arrayTeste, [1, 2, 3]);
+
+//A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
+// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
+// Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
+// Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
+
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz' );
+assert.strictEqual(myFizzBuzz(9), 'fizz' );
+assert.strictEqual(myFizzBuzz(10), 'buzz' );
+assert.strictEqual(myFizzBuzz(11), 11 );
+assert.strictEqual(myFizzBuzz('5'),  false);
+
+//Compare dois objetos para verificar se são idênticos ou não
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(obj1, obj2);
+assert.notDeepStrictEqual(obj1, obj3);
+
+
+
+
+
